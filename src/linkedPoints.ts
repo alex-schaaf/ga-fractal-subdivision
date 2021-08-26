7209693bad5890b70b502c794db3f9a07fffc990
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-08-26 18:32:54 +0200

    ♻️ refactor into LinkedPoints class
    + drawLines, drawPoints methods
    + subdivide(n) method

---
+++ src/linkedPoints.ts
@@ -0,0 +1,50 @@
+import p5 from 'p5'
+
+export interface IPoint {
+  x: number
+  y: number
+  next: IPoint | null
+}
+
+export class LinkedPoints {
+  head: IPoint | null
+
+  constructor(head: IPoint | null = null) {
+    this.head = head
+  }
+
+  subdivide(n: number): void {
+    if (!this.head || !this.head.next) return
+
+    for (let i = 0; i < n; i++) {
+      let point = this.head
+
+      while (point.next) {
+        // TODO add optional arg of type func that returns a (random) value to x, y
+        const newX = 0.5 * (point.x + point.next.x)
+        const newY = 0.5 * (point.y + point.next.y)
+        let midPoint = { x: newX, y: newY, next: point.next } as IPoint
+
+        let nextPoint = point.next
+        point.next = midPoint
+        point = nextPoint
+      }
+    }
+  }
+
+  drawLines(p: p5): void {
+    let point = this.head
+    while (point && point.next) {
+      p.line(point.x, point.y, point.next.x, point.next.y)
+      point = point.next
+    }
+  }
+
+  drawPoints(p: p5): void {
+    let point = this.head
+    while (point) {
+      p.point(point.x, point.y)
+      point = point.next
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,41 @@
 import p5 from 'p5'
 import "./styles.scss";
-
-
-interface IPoint {
-  x: number
-  y: number
-  next: IPoint | null
-}
+import { IPoint, LinkedPoints } from './linkedPoints'
 
 const canvasX = 800
 const canvasY = 600
 
-
-
-const getDistanceXY = (a: IPoint, b: IPoint): {x: number, y: number} => {
-  return {x: Math.abs(a.x - b.x), y: Math.abs(a.y - b.y)}
-}
-
-
-const subdivide = (start: IPoint, n: number): void => {
-  if (!start.next) {
-    return
-  }
-  
-  for (let i = 0; i < n; i++) {
-    let point = start
-    while (point.next) {
-      let nextPoint = point.next
-      const distance = getDistanceXY(nextPoint, point)
-  
-      const newX = 0.5 * (point.x + nextPoint.x)
-      const newY = 0.5 * (point.y + nextPoint.y)
-  
-      let newPoint = {x: newX, y: newY} as IPoint
-      // squeeze in-between points
-      newPoint.next = nextPoint
-      point.next = newPoint
-      
-      point = nextPoint
-    }
-  }
-}
-
 const sketch = function (p: p5) {
   const end = {x: canvasX * 0.9, y: canvasY / 2} as IPoint
   const start = {x: canvasX * 0.1, y: canvasY / 2, next: end} as IPoint
+
+  let points = new LinkedPoints(start)
   
   p.setup = () => {
     const canvas = p.createCanvas(canvasX, canvasY)
     canvas.parent('p5-canvas')
     p.stroke(255)
-
-    subdivide(start, 2)
-
-    draw(start)
-    
+    p.frameRate(10)
   }
 
-  const draw = (point: IPoint) => {
-    while (point) {
-      p.strokeWeight(5)
-      p.point(point.x, point.y)
-      if (point.next) {
-        p.strokeWeight(1)
-        p.line(point.x, point.y, point.next.x, point.next.y)
-      }
-      point = point.next
+
+  let nIter = 0
+  p.draw = () => {
+    points.head.next = end
+    
+    points.subdivide(3)
+    p.strokeWeight(1)
+    points.drawLines(p)
+    p.strokeWeight(5)
+    points.drawPoints(p)
+    
+    nIter++
+
+    if (nIter > 20) {
+      p. clear()
+      nIter = 0
     }
   }
 }
 
 new p5(sketch)
-
-
-
-
-
-
-
